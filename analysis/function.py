7c52a20ca823d5d7f3d22d7dba3d9863275ca406
Michael <mikrlynch@gmail.com>
2020-03-25 22:31:49 -0400

    Got docker compose to work

--- analysis/function.py
+++ analysis/function.py
@@ -1,12 +1,12 @@
 from pymongo import MongoClient
 
-mongo = MongoClient("mongodb://172.17.0.2:27017").testdb
+mongo = MongoClient("mongodb://mongo_database/user_database")
 
 
 def readDB(idx):
-    data = mongo.posts.find_one({'_id':idx})
+    data = mongo.user_database.posts.find_one({'_id':idx})
 
-    message, timestamp = data['message'], data['time']
+    message, timestamp = data["message"], data["time"]
     return (message, timestamp)
 
 def getResponse(idx):
--- app/app.py
+++ app/app.py
@@ -1,12 +1,21 @@
 import json
-from flask_pymongo import PyMongo
 from flask import Flask
 from flask import request, render_template
+from flask_pymongo import PyMongo
 from analysis.function import getResponse
 from datetime import date
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb://172.17.0.2:27017/testdb"
+
+#app.config['MONGO_HOST'] = 'mongo_database'
+#app.config['MONGO_PORT'] = '27017'
+#app.config['MONGO_DBNAME'] = 'user_database'
+#app.config['MONGO_USERNAME'] = 'application'
+#app.config['MONGO_PASSWORD'] = 'apppassword'
+#app.config['MONGO_AUTH_SOURCE'] = 'admin'
+#app.config["MONGO_URI"] = 'mongodb://mongo_database'
+app.config["MONGO_URI"] = 'mongodb://mongo_database/user_database'
+#app.config["MONGO_URI"] = 'mongodb://root:password@mongo_database:27017/user_database?authSource=admin'
 mongo=PyMongo(app)
 
 @app.route('/', methods=['GET'])
@@ -18,6 +27,7 @@ def my_form_post():
 
     message, timestamp = request.form['msg'], date.today()
     post = {"message":message, "time":timestamp.__str__()}
+
     post_id = mongo.db.posts.insert_one(post).inserted_id
 
     response = getResponse(post_id)
